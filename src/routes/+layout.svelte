da526d502e22ac6c9286ad74d976217d0c688696
charles <charles.braquet@e.email>
2024-02-23 18:36:13 +0100

    initial commit of link_issue

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
+<h1>Testing the bug</h1>
+<nav>
+	<!--a href={`${base}/`}>home</a-->
+	<a href="{base}">home</a>
+</nav>
+<main>
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { base } from '$app/paths';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Try <a href="{base}/sub1/sub2">sub2</a></p>
+<p>Try <a href="{base}/sub1">sub1</a></p>
+
